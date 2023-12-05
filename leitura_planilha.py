26fdc2b2b350595d2f853af7ae71d60a73b55911
Roger Oliveira <41457384+Rjj18@users.noreply.github.com>
2023-12-05 13:54:11 -0300

    Update leitura_planilha.py

--- leitura_planilha.py
+++ leitura_planilha.py
@@ -1,23 +1,13 @@
 import pandas as pd
 import openpyxl as openpyxl
 
-def ler_arquivo_excel(caminho_arquivo):
-    """
-    Função para ler um arquivo Excel usando o Pandas.
+def read_excel_file(caminho_arquivo):
 
-    Parameters:
-    - caminho_arquivo (str): O caminho do arquivo Excel.
-
-    Returns:
-    """
+//Parameters: - excel file path.
     try:
-        # Utiliza a função read_excel do Pandas para ler o arquivo Excel
         dataframe = pd.read_excel(caminho_arquivo)
-        
-        # Retorna o DataFrame resultante
         return dataframe
     
     except Exception as e:
-        # Em caso de erro, imprime uma mensagem e retorna None
         print(f"Erro ao ler o arquivo Excel: {e}")
         return None
